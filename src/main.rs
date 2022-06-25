e0be6f34618e35b1738595fe11f2e82fd8590636
Mayu <mayuran.ganesathas@gmail.com>
2022-06-25 18:16:45 -0400

    mg- add json responses

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,31 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, web, Responder, Result};
+use serde::Serialize;
 
-
-
-#[actix_web::main] // or #[tokio::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new().service(greet)
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+#[derive(Serialize)]
+//Struct for value return of score
+struct Score {
+    score_value: u8,
 }
 
-#[get("/hello/{name}")]
-async fn greet(name: web::Path<String>) -> impl Responder {
-    format!("Hello {}!", name)
+
+//Instantiates web server 
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    use actix_web::{App, HttpServer};
+
+    HttpServer::new(|| App::new().service(index))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
+
+
+
+//Handles GET request to /req/score and returns score
+#[get("/req/score")]
+async fn index() -> Result<impl Responder> {
+    let obj = Score {
+        score_value: 12, //Holds arbitray value pass score value into here
+    };
+    Ok(web::Json(obj))
 }
